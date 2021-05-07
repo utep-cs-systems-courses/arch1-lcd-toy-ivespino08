e23a0b8236fc11505d0734ed8eb6a586ffc8ee45
ivespino08 <ivespino08@gmail.com>
2021-05-06 21:17:52 -0600

    interrups working

--- game/game.c
+++ game/game.c
@@ -2,8 +2,11 @@
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
+#include <p2switches.h>
 #include <shape.h>
 #include <abCircle.h>
+
+#define GREEN_LED BIT6
 
 AbRect rect0 =  {abRectGetBounds, abRectCheck, {1,12}};
 
@@ -33,12 +36,53 @@ Layer layer2 = {
   &layer1
 };
 
+int redrawScreen = 1;
+
 int main(){
   configureClocks();
   lcd_init();
   shapeInit();
+  p2sw_init(1);
+
+  enableWDTInterrupts();
+  or_sr(0x8);
 
   clearScreen(COLOR_RED);
-  layerDraw(&layer2);
+  
+  for(;;){
+    while(!redrawScreen){
+      P1OUT &= ~GREEN_LED;
+      or_sr(0x10);
+    }
+    P1OUT |= GREEN_LED;
+    redrawScreen = 0;
+    layerDraw(&layer2);
+  }
+}
 
+void wdt_c_handler()
+{
+  static char state = 0;
+  static short count = 0;
+  P1OUT |= GREEN_LED;
+  count++;
+  if(count == 1000){
+    switch(state){
+    case 0:
+      bgColor = COLOR_BLUE;
+      state = 1;
+      break;
+    case 1:
+      bgColor = COLOR_VIOLET;
+      state = 2;
+      break;
+    case 2:
+      bgColor = COLOR_RED;
+      state = 0;
+      break;
+    }
+    redrawScreen = 1;
+    count = 0;
+  }
+  P1OUT &= ~GREEN_LED;
 }
